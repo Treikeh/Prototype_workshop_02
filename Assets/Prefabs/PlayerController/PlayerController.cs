2c8871dda9cea900b7da403562c50eaa94d77bd5
Treikeh <73736974+Treikeh@users.noreply.github.com>
2025-03-04 12:05:09 +0100

    Simple player controller

---
+++ Assets/Prefabs/PlayerController/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 200f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private bool isGrounded = false;
+    private float moveInput;
+    private Rigidbody2D rBody;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        Debug.Log(isGrounded);
+        // Move left and right
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rBody.linearVelocityX = moveInput * moveSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rBody.linearVelocityY = jumpForce;
+        }
+    }
+}
